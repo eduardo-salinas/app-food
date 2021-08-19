570ee88dfdb2fcf44f6b1fbd347facddf9cd2794
Eduardo Salinas <eduardo.salinas1@outlook.com>
2021-08-19 13:51:33 -0300

    update

--- api/src/request/recipeID.js
+++ api/src/request/recipeID.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 
 const axios = require('axios').default;
-const { API_KEY } = process.env;
+const { API_KEY1, API_KEY2, API_KEY3, API_KEY4, API_KEY5 } = process.env
 
+let index = 1;
+let apiKey;
 
 const recipeId = async (id) => {
+    switch (index) {
+        case 1: apiKey = API_KEY1;
+        case 2: apiKey = API_KEY2;
+        case 3: apiKey = API_KEY3;
+        case 4: apiKey = API_KEY4;
+        case 5: apiKey = API_KEY5;
+        default: apiKey = API_KEY1;
+    }
     try {
-        const recipe = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`);
+        const recipe = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`);
         const r = recipe.data;
         const recipeId = {
             id: r.id,
@@ -22,6 +32,11 @@ const recipeId = async (id) => {
         return recipeId;
 
     } catch (e) {
+        if (index >= 5) {
+            index = 1;
+        } else {
+            index++
+        }
         return false;
     };
 }
